1beb1e184c67a2636cc336687b6e89c1d0e4367b
Adriana Scrobote <adriana_100000@hotmail.com>
2022-09-04 20:39:06 -0300

    First commit

---
+++ src/test/java/com/br/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.crud-java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class crud-javaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
